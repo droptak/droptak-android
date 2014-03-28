0166b8219f694ab0d7a968c6ae297b0015d513e6
Kyle Potts <kyle@kptechblog.com>
2014-03-28 00:24:57 -0400

    Added loginTask

--- maptak/app/src/main/java/edu/purdue/maptak/admin/fragments/LoginFragment.java
+++ maptak/app/src/main/java/edu/purdue/maptak/admin/fragments/LoginFragment.java
@@ -2,24 +2,29 @@ package edu.purdue.maptak.admin.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.PendingIntent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import edu.purdue.maptak.admin.R;
+import edu.purdue.maptak.admin.Tasks.LoginTask;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
+import com.google.android.gms.auth.*;
 import android.content.IntentSender.*;
+import java.io.IOException;
 import android.content.Intent;
 import android.widget.Toast;
 
 public class LoginFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, View.OnClickListener {
     /* Track whether the sign-in button has been clicked so that we know to resolve
-* all issues preventing sign-in without waiting.
-*/
+    * all issues preventing sign-in without waiting.
+    */
     private boolean mSignInClicked;
 
     /* Store the connection result from onConnectionFailed callbacks so that we can
@@ -39,6 +44,11 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
    */
 
     private boolean mIntentInProgress;
+    private PendingIntent mSignInIntent;
+    private static final int STATE_DEFAULT = 0;
+    private static final int STATE_SIGN_IN = 1;
+    private static final int STATE_IN_PROGRESS = 2;
+    private int mSignInProgress;
 
 
     /**
@@ -61,25 +71,26 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
 
     private void resolveSignInError() {
         Log.d("debug","resolve");
-        if (mConnectionResult.hasResolution()) {
+        if (mSignInIntent!= null) {
             try {
-                mIntentInProgress = true;
+                mSignInProgress = STATE_IN_PROGRESS;
                 mConnectionResult.startResolutionForResult(this.getActivity(),RC_SIGN_IN);
             } catch (SendIntentException e) {
                 // The intent was canceled before it was sent.  Return to the default
                 // state and attempt to connect to get an updated ConnectionResult.
-                mIntentInProgress = false;
+                mSignInProgress =STATE_SIGN_IN;
                 mGoogleApiClient.connect();
             }
         }
     }
 
     public void onClick(View view) {
-        Log.d("Debug","Goole+ Signin Pressed");
-        if (view.getId() == R.id.sign_in_button
-                && !mGoogleApiClient.isConnecting()) {
-            mSignInClicked = true;
-            resolveSignInError();
+        if(!mGoogleApiClient.isConnecting()) {
+            Log.d("Debug", "Goole+ Signin Pressed");
+            if (view.getId() == R.id.sign_in_button) {
+                mSignInClicked = true;
+                resolveSignInError();
+            }
         }
     }
 
@@ -97,17 +108,19 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
     }
 
     public void onConnectionFailed(ConnectionResult result) {
-        if (!mIntentInProgress) {
-            // Store the ConnectionResult so that we can use it later when the user clicks
-            // 'sign-in'.
+        if (mSignInProgress != STATE_IN_PROGRESS) {
+            // We do not have an intent in progress so we should store the latest
+            // error resolution intent for use when the sign in button is clicked.
+            mSignInIntent = result.getResolution();
             mConnectionResult = result;
+        }
 
-            if (mSignInClicked) {
-                // The user has already clicked 'sign-in' so we attempt to resolve all
-                // errors until the user is signed in, or they cancel.
+            if (mSignInProgress == STATE_SIGN_IN) {
+                // STATE_SIGN_IN indicates the user already clicked the sign in button
+                // so we should continue processing errors until the user is signed in
+                // or they click cancel.
                 resolveSignInError();
             }
-        }
     }
 
 
@@ -124,7 +137,18 @@ public class LoginFragment extends Fragment implements GoogleApiClient.OnConnect
            email = Plus.AccountApi.getAccountName(mGoogleApiClient);
         }
         Toast.makeText(this.getActivity(),personName+ " logged in " + email, Toast.LENGTH_LONG).show();
+        getStoreToken();
         
+
+    }
+
+    public void getStoreToken(){
+        LoginTask loginTask = new LoginTask(this.getActivity().getBaseContext(),mGoogleApiClient);
+        try {
+            loginTask.execute();
+        } catch (Exception e){
+
+        }
 
     }
 
