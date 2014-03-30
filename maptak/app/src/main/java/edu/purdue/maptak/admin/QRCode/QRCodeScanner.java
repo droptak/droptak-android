3c8b44f21b2c20456536549f5dfbbbc94c9d0a18
garrett2 <garrett2@purdue.edu>
2014-03-30 15:48:31 -0400

    Updated the QRCode Fragment to support actions after the scan

--- maptak/app/src/main/java/edu/purdue/maptak/admin/QRCode/QRCodeScanner.java
+++ maptak/app/src/main/java/edu/purdue/maptak/admin/QRCode/QRCodeScanner.java
@@ -1,6 +1,9 @@
 package edu.purdue.maptak.admin.QRCode;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 
 /**
  * Created by tylorgarrett on 3/29/14.
@@ -19,5 +22,14 @@ public class QRCodeScanner {
         integrator.initiateScan();
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            // call the QRCodeFragment
+            // display the code that was displayed
+        } else {
+            // display an error, say the code couldn't be read or something like that
+        }
+    }
 
 }
