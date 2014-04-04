9b1ef69647013d92a5caa7c69b47c940f6768ed4
Michael Hockerman <mike@hockerman.com>
2014-04-04 00:10:22 -0400

    Added dialog fragment for map chooser

--- maptak/app/src/main/java/edu/purdue/maptak/admin/fragments/DrawerFragment.java
+++ maptak/app/src/main/java/edu/purdue/maptak/admin/fragments/DrawerFragment.java
@@ -1,16 +1,16 @@
 package edu.purdue.maptak.admin.fragments;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.purdue.maptak.admin.R;
@@ -19,7 +19,7 @@ import edu.purdue.maptak.admin.data.MapTakDB;
 import edu.purdue.maptak.admin.tasks.LoginTask;
 import edu.purdue.maptak.admin.widgets.MapListItemAdapter;
 
-public class DrawerFragment extends Fragment {
+public class DrawerFragment extends Fragment implements View.OnClickListener {
 
     /** Whether or not the user is logged in */
     private boolean isLoggedIn = false;
@@ -30,15 +30,10 @@ public class DrawerFragment extends Fragment {
         String id = getActivity().getPreferences(Context.MODE_PRIVATE).getString(LoginTask.PREF_USER_LOGIN_TOKEN, "");
 
         View v = null;
-        //if (id == "") {
-            // User is not logged in
-            v = inflater.inflate(R.layout.fragment_drawer_notloggedin, container, false);
-        //} else {
-            // User is logged in
-        //}
+        v = inflater.inflate(R.layout.fragment_drawer, container, false);
 
         // Get widgets on the screen
-        Button buQR = (Button) v.findViewById(R.id.drawer_bu_qrcode);
+        Button buCreateMap = (Button) v.findViewById(R.id.drawer_bu_createmap);
         ListView lvMaps = (ListView) v.findViewById(R.id.drawer_lv_maplist);
 
         // Get user's stored maps
@@ -49,5 +44,28 @@ public class DrawerFragment extends Fragment {
         lvMaps.setAdapter(new MapListItemAdapter(getActivity(), maps));
 
         return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.drawer_bu_createmap:
+
+                String[] items = new String[]{ "Create a new map", "Scan a QR Code", "Search"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Add a Map")
+                    .setItems(items, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                                // The 'which' argument contains the index position
+                                // of the selected item
+                            }
+                        });
+
+                builder.create();
+                break;
+        }
+
     }
 }
